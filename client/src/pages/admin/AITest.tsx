474dc3967a5fe5a3fc8b19ea4e0d21f17361632e
Hemant <rautanhemu@gmail.com>
2026-06-13 03:07:23 +0530

    fixed npm run builds

--- client/src/pages/admin/AITest.tsx
+++ client/src/pages/admin/AITest.tsx
@@ -14,7 +14,7 @@ import {
 } from '@heroicons/react/24/outline';
 import FinalsLogo from '../../components/FinalsLogo';
 import AdminPageHeader from '../../components/admin/AdminPageHeader';
-import { adminNavActive, adminNavInactive } from '../../components/admin/adminNavStyles';
+// import { adminNavActive, adminNavInactive } from '../../components/admin/adminNavStyles';
 
 const AITest: React.FC = () => {
   const navigate = useNavigate();
